0cb8e996479b1c2ec35b3ecf2efafae85cb41f7b
bls20AWS <benjasl@a483e7468bf4.ant.amazon.com>
2020-11-24 10:28:11 +0000

    innitial commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# sfn-apigw-example2
+# Example Step functions API Gateway integration 
 
 This project contains source code and supporting files for a serverless application that you can deploy with the SAM CLI. It includes the following files and folders:
 
---
+++ functions/stock-seller/app.js
@@ -0,0 +1,40 @@
+/*  
+SPDX-FileCopyrightText: 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: MIT-0 
+*/
+
+const crypto = require("crypto");
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max)) + 1;
+}
+
+/**
+ * Sample Lambda function which mocks the operation of selling a random number of shares for a stock.
+ * For demonstration purposes, this Lambda function does not actually perform any  actual transactions. It simply returns a mocked result.
+ * 
+ * @param {Object} event - Input event to the Lambda function
+ * @param {Object} context - Lambda Context runtime methods and attributes
+ *
+ * @returns {Object} object - Object containing details of the stock selling transaction
+ * 
+ */
+exports.lambdaHandler = async (event, context) => {
+    // Get the price of the stock provided as input
+    const body = JSON.parse(event.body)
+    const stock_price = body.stock_price
+
+    var date = new Date();
+    // Mocked result of a stock selling transaction
+    let transaction_result = {
+        'id': crypto.randomBytes(16).toString("hex"), // Unique ID for the transaction
+        'price': stock_price.toString(), // Price of each share
+        'type': "sell", // Type of transaction(buy/ sell)
+        'qty': getRandomInt(10).toString(),  // Number of shares bought / sold(We are mocking this as a random integer between 1 and 10)
+        'timestamp': date.toISOString(),  // Timestamp of the when the transaction was completed
+    }
+    return { 
+        statusCode: 200,
+        body:JSON.stringify(transaction_result ),
+        }
+};
